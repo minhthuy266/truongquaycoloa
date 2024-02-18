5e08baae5766e8160318af6980e65ef3b716b724
Thuy Nguyen <thuy.nguyenminh266@gmail.com>
2024-02-18 20:58:27 +0700

    Update about screen

--- src/screens/AboutScreen/index.jsx
+++ src/screens/AboutScreen/index.jsx
@@ -2,13 +2,12 @@ const AboutScreen = () => {
   return (
     <div className="text-black container mx-auto mt-[120px] global-text-base custom-min-height">
       <div className="container pb-24">
-        <h1>Giới thiệu</h1>
         <p>Mọi thông tin chi tiết xin liên hệ:</p>
         <p>
-          Mr. Lê Anh Phương - Phó Phòng kinh doanh và Sản xuất chương trình -
+          Mr. Lê Anh Phương - Trưỏng phòng kỹ thuật và Sản xuất chương trình -
           SĐT: 094.665.3988,
         </p>
-        <p>Mr. Nguyễn Thế Anh - Phụ Trách Phòng Kế Hoạch - SĐT: 0965.510.795</p>
+        <p>Mr. Nguyễn Thế Anh - Nhân viên Kinh doanh - SĐT: 0965.510.795</p>
       </div>
     </div>
   );
